0425c70213b752fcc5774bf539f60c390be6d2c9
LeoMcBills <leokinyera9@gmail.com>
2022-12-17 14:21:20 +0300

    Prompt a user for integer input and carry out an operation n + nn + nnn on it

---
+++ basics1/accept_and_add.py
@@ -0,0 +1,5 @@
+'''This program takes input of an integer and carries out an operation n + nn + nnn on it'''
+
+n = int(input("Enter number(integer): "))
+value = n + n*n + n*n*n
+print("The value of the computation is ", value)
