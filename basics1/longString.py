2f8ef33503b5c8a6109eb603cc6345666d8518c2
LeoMcBills <leokinyera9@gmail.com>
2022-12-17 14:13:29 +0300

    Print a string with many characters

---
+++ basics1/longString.py
@@ -0,0 +1,6 @@
+'''A python program that prints a string of very many characters on screen
+ using the print function'''
+
+print(''' \nHi there, this is Leo and I'm so glad that you checked my repo. This project is intended to tackle alot
+ in python for purposes of becoming efficient and good at the language from its basic to the most advanced.
+ thank you, I hope you do practice most of these projects if not then practice all of them Otherwise, let's jump into it now.\n''')
